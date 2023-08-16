cc87081f143cd1ffa353de313472ad4f0ed998bf
qinjie <mark.qj@gmail.com>
2023-08-16 07:46:27 +0000

    test13

--- s3-bucket.tf
+++ s3-bucket.tf
@@ -2,7 +2,9 @@ module "s3_bucket" {
   source = "terraform-aws-modules/s3-bucket/aws"
 
   bucket = "my-qinjie-s3-bucket"
-  acl    = "private"
+
+  control_object_ownership = true
+  object_ownership         = "BucketOwnerPreferred"
 
   versioning = {
     enabled = true
